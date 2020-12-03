b5f6c92354a24451f9fd02ba9513644d8949f708
Petr Yudin <udin.petr@gmail.com>
2020-12-03 21:13:34 +0300

    функция, которая вносит изменения в таблицу

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,21 @@
  * @param {Element} table
  */
 function highlight(table) {
+    for (let row of table.rows) {
+        if (row.cells[3].dataset.available === 'true') {
+            row.classList.add('available');
+        } else if  (row.cells[3].dataset.available === 'false') {
+            row.classList.add('unavailable');
+        } else {
+            row.hidden = true;
+        }
+        if (row.cells[2].innerHTML === "m") {
+            row.classList.add("male");
+          } else { 
+            row.classList.add("female"); 
+          }
+          if (row.cells[1].innerHTML < 18) {
+            row.style.textDecoration = "line-through";
+          }
+    }
 }
